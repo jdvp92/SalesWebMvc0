0af9851630082b4e60a892097fadbde23c670948
Jonathan Silva <j.dvp92@outlook.com>
2023-03-10 14:18:46 -0300

    Validation

--- SalesWebMvc0/Models/Seller.cs
+++ SalesWebMvc0/Models/Seller.cs
@@ -10,20 +10,32 @@ namespace SalesWebMvc0.Models
     {
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
+
         public Department Department { get; set; }
+
 
         [Display(Name = "Department Id")]
         public int DepartmentId { get; set; }
@@ -44,7 +56,7 @@ namespace SalesWebMvc0.Models
             Department = department;
         }
 
-        public void AddSales (SalesRecord sr)
+        public void AddSales(SalesRecord sr)
         {
             Sales.Add(sr);
         }
