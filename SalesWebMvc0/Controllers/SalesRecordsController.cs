cdc6086a58712b9ae5de021fe520717681fb69d2
Jonathan Silva <j.dvp92@outlook.com>
2023-03-14 14:02:04 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc0/Controllers/SalesRecordsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace SalesWebMvc0.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
