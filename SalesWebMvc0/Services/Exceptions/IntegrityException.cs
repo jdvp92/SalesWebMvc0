378532658cba5617a0ba734106fce4ed7b075af3
Jonathan Silva <j.dvp92@outlook.com>
2023-03-14 13:53:22 -0300

    Exception handling for delete (referential integrity)

---
+++ SalesWebMvc0/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc0.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message)
+        { 
+        }
+    }
+}
